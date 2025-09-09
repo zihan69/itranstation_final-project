64070dc75062e84fb8969674166ef7d93dbf0c64
zihan69 <zihanfaisalahmed@gmail.com>
2025-09-09 11:52:55 +0600

    save2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,27 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-// Pages
-import Home from "./pages/Home";
-import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div>
-      {/* Navbar */}
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container">
-          <Link className="navbar-brand" to="/">Inventory</Link>
-          <div>
-            <Link className="btn btn-primary me-2" to="/login">Login</Link>
-            <Link className="btn btn-success" to="/signup">Signup</Link>
-          </div>
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+        <Link className="navbar-brand" to="/">Inventory</Link>
+        <div className="navbar-nav">
+          <Link className="nav-link" to="/login">Login</Link>
+          <Link className="nav-link" to="/signup">Signup</Link>
         </div>
       </nav>
 
-      {/* Routes */}
-      <div className="container mt-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </div>
-    </div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<h1 className="text-center mt-5">🏠 Welcome to Inventory App</h1>} />
+      </Routes>
+    </>
   );
 }
 
